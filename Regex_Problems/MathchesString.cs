8ad7ead15ee6df88c0e221f23ae24ef94be78995
Shivani21-hude <shivani21hude@gmail.com>
2023-03-28 17:32:49 +0530

    [Shivani]-Check the string is match or not

---
+++ Regex_Problems/MathchesString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex_Problems
+{
+    internal class MathchesString
+    {
+       
+        public void Matchesstring()
+        {
+            //step 1: create object of regex
+              Regex regex = new Regex(@"^[a]{1}[b]{2,3}$");
+
+            Console.WriteLine("Enter a string");
+            string word=Console.ReadLine();
+
+            // Step 2: call Match on Regex instance.
+            Match match = regex.Match(word);
+
+            // Step 3: test for Success.
+            if (match.Success)
+            {
+                Console.WriteLine("It is VALID : ");
+            }
+            else
+            {
+                Console.WriteLine("string is not valid");
+            }
+
+        }
+
+    }
+}
---
+++ Regex_Problems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Problems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MathchesString Valid = new MathchesString();
+            Valid.Matchesstring();
+        }
+    }
+}
